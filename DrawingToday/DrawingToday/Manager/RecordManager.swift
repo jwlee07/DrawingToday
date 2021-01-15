1c6e7dd42ae81556d094cd4c7dee15d83f7b25b9
jwlee07 <apzk21413@gmail.com>
2021-01-15 18:29:56 +0900

    feat: 미디어 처리 상태값 확인 구현 완료, 비디오 레코딩 기능 구현 중

--- DrawingToday/DrawingToday/Manager/RecordManager.swift
+++ DrawingToday/DrawingToday/Manager/RecordManager.swift
@@ -29,4 +29,39 @@ class RecordManager {
             recorder?.prepare(configuration)
         }
     }
+    /// 미디어 파일 처리에 대한 알림 메세지 Present
+    func exportMessage(success: Bool, viewController: BaseARViewController, status: PHAuthorizationStatus) {
+        if success {
+            print("성공")
+        } else if status == .denied || status == .restricted || status == .notDetermined {
+            print("사진 라이브러리에 대한 액세스 허용이 되어 있지 않습니다.")
+            // 사진 라이브러리 설정 변경을 하기 위한 설정 진입
+            guard let settingUrl = URL(string: UIApplication.openSettingsURLString) else { return }
+            guard UIApplication.shared.canOpenURL(settingUrl) else { return }
+            UIApplication.shared.open(settingUrl) { success in
+                print("success : ", success)
+            }
+        } else {
+            print("미디어 파일 Export 시 오류 발생")
+        }
+    }
+    /// 비디오 레코딩 (시간 필요)
+    func videoRecordering(sender: UIButton, recorder: RecordAR?, viewController: BaseARViewController) {
+        switch recorder?.status {
+        case .readyToRecord:
+            sender.setTitle("영상녹화멈춤", for: .normal)
+            recordingQueue.async {
+                recorder?.record(forDuration: 10) { path in
+                    recorder?.export(video: path) { saved, status in
+                        DispatchQueue.main.sync {
+                            sender.setTitle("영상녹화저장", for: .normal)
+                            self.exportMessage(success: saved, viewController: viewController, status: status)
+                        }
+                    }
+                }
+            }
+        default:
+            break
+        }
+    }
 }
